58087ceb4404d0f23ee722749b20565eac3d115e
fanyingdong <fanyd@ccccit.com.cn>
2024-01-31 15:10:41 +0800

    ✨ feat: 增加同步代码库提交同步钩子

---
+++ install_hooks.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+
+def install_hook(script_name):
+    hooks_dir = os.path.join('.git', 'hooks')
+    custom_hooks_dir = 'custom_hooks'
+    script_path = os.path.join(custom_hooks_dir, script_name)
+    target_path = os.path.join(hooks_dir, script_name)
+
+    if os.path.isfile(script_path):
+        shutil.copy(script_path, target_path)
+        print(f"{script_name} hook installed.")
+    else:
+        print(f"Error: {script_path} does not exist.")
+
+
+if __name__ == "__main__":
+    install_hook('post-commit')
