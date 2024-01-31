6b6d1d418ca615d8c7fd43daa5b21c31b28bc14b
fanyingdong <fanyd@ccccit.com.cn>
2024-01-31 15:59:43 +0800

    feat: hook test

--- install_hooks.py
+++ install_hooks.py
@@ -3,7 +3,6 @@ import shutil
 
 
 def install_hook(script_name):
-    # install the hook
     hooks_dir = os.path.join('.git', 'hooks')
     custom_hooks_dir = 'custom_hooks'
     script_path = os.path.join(custom_hooks_dir, script_name)
